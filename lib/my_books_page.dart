7cedf297aeb8a44a528116453cbb846040243d24
sricharan8462 <sricharan8462@yahoo.com>
2025-04-30 21:18:11 -0400

    Add Firestore Integration and Load Books into Tabs

--- lib/my_books_page.dart
+++ lib/my_books_page.dart
@@ -1,31 +1,79 @@
 import 'package:flutter/material.dart';
+import 'firestore_service.dart';
 
-class MyBooksPage extends StatelessWidget {
+class MyBooksPage extends StatefulWidget {
   const MyBooksPage({Key? key}) : super(key: key);
 
   @override
+  State<MyBooksPage> createState() => _MyBooksPageState();
+}
+
+class _MyBooksPageState extends State<MyBooksPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  List<Map<String, dynamic>> currentlyReading = [];
+  List<Map<String, dynamic>> wantToRead = [];
+  List<Map<String, dynamic>> finishedReading = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    fetchMyBooks();
+  }
+
+  Future<void> fetchMyBooks() async {
+    currentlyReading = await FirestoreService.getBooksFromList(
+      'currentlyReading',
+    );
+    wantToRead = await FirestoreService.getBooksFromList('wantToRead');
+    finishedReading = await FirestoreService.getBooksFromList(
+      'finishedReading',
+    );
+    setState(() {});
+  }
+
+  Widget buildList(List<Map<String, dynamic>> books) {
+    return books.isEmpty
+        ? Center(child: Text('No books here.'))
+        : ListView.builder(
+          itemCount: books.length,
+          itemBuilder:
+              (context, index) => ListTile(
+                title: Text(books[index]['title'] ?? 'No Title'),
+                subtitle: Text(books[index]['authors'] ?? ''),
+              ),
+        );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('My Books'),
-          backgroundColor: Colors.deepPurple,
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Currently Reading'),
-              Tab(text: 'Want to Read'),
-              Tab(text: 'Finished Reading'),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            Center(child: Text('Currently Reading')),
-            Center(child: Text('Want to Read')),
-            Center(child: Text('Finished Reading')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Books'),
+        backgroundColor: Colors.deepPurple,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Currently Reading'),
+            Tab(text: 'Want to Read'),
+            Tab(text: 'Finished Reading'),
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          buildList(currentlyReading),
+          buildList(wantToRead),
+          buildList(finishedReading),
+        ],
       ),
     );
   }
